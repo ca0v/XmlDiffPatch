e89e9af144faebf25e86449eb4c82810645c2b36
ca0v <coreyalix@hotmail.com>
2020-08-03 11:33:14 -0400

    adds csproj

--- XmlDiffPatch.View/OperationDescriptor.cs
+++ XmlDiffPatch.View/OperationDescriptor.cs
@@ -4,26 +4,36 @@
 // MVID: 0D4C313F-7E60-4DB8-9CA5-4749E3A923DE
 // Assembly location: C:\development\Trunk\UnitTests\NugetRepository\NUnit3\packages\XMLDiffPatch.1.0.8.28\lib\net\XmlDiffPatch.View.dll
 
+using System.Xml;
+
 namespace Microsoft.XmlDiffPatch
 {
-  internal class OperationDescriptor
-  {
-    private int _opid;
-    internal OperationDescriptor.Type _type;
-    internal XmlDiffPathMultiNodeList _nodeList;
-
-    public OperationDescriptor(int opid, OperationDescriptor.Type type)
+    internal class OperationDescriptor
     {
-      this._opid = opid;
-      this._type = type;
-      this._nodeList = new XmlDiffPathMultiNodeList();
-    }
+        private int _opid;
+        internal OperationDescriptor.Type _type;
+        internal XmlDiffPathMultiNodeList _nodeList;
 
-    internal enum Type
-    {
-      Move,
-      NamespaceChange,
-      PrefixChange,
+        public OperationDescriptor(int opid, OperationDescriptor.Type type)
+        {
+            this._opid = opid;
+            this._type = type;
+            this._nodeList = new XmlDiffPathMultiNodeList();
+        }
+
+        internal enum Type
+        {
+            Move,
+            NamespaceChange,
+            PrefixChange,
+        }
+
+        internal virtual void WriteTo(XmlWriter xmlWriter)
+        {
+            xmlWriter.WriteStartElement("xd", "descriptor", "http://schemas.microsoft.com/xmltools/2002/xmldiff");
+            xmlWriter.WriteAttributeString("opid", this._opid.ToString());
+            xmlWriter.WriteAttributeString("type", System.Enum.GetName(typeof(Type), this._type));
+            xmlWriter.WriteEndElement();
+        }
     }
-  }
 }
--- XmlDiffPatch/OperationDescriptor.cs
+++ XmlDiffPatch/OperationDescriptor.cs
@@ -1,31 +1,31 @@
-﻿// Decompiled with JetBrains decompiler
-// Type: Microsoft.XmlDiffPatch.OperationDescriptor
-// Assembly: XmlDiffPatch, Version=1.0.8.28, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a
-// MVID: 2B32D548-922A-4A84-B9AD-FF8E573DAC90
-// Assembly location: C:\development\Trunk\UnitTests\NugetRepository\NUnit3\packages\XMLDiffPatch.1.0.8.28\lib\net\XmlDiffPatch.dll
+﻿//// Decompiled with JetBrains decompiler
+//// Type: Microsoft.XmlDiffPatch.OperationDescriptor
+//// Assembly: XmlDiffPatch, Version=1.0.8.28, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a
+//// MVID: 2B32D548-922A-4A84-B9AD-FF8E573DAC90
+//// Assembly location: C:\development\Trunk\UnitTests\NugetRepository\NUnit3\packages\XMLDiffPatch.1.0.8.28\lib\net\XmlDiffPatch.dll
 
-using System.Xml;
+//using System.Xml;
 
-namespace Microsoft.XmlDiffPatch
-{
-  internal abstract class OperationDescriptor
-  {
-    protected ulong _operationID;
-    internal OperationDescriptor _nextDescriptor;
+//namespace Microsoft.XmlDiffPatch
+//{
+//  internal abstract class OperationDescriptor
+//  {
+//    protected ulong _operationID;
+//    internal OperationDescriptor _nextDescriptor;
 
-    internal OperationDescriptor(ulong opid)
-    {
-      this._operationID = opid;
-    }
+//    internal OperationDescriptor(ulong opid)
+//    {
+//      this._operationID = opid;
+//    }
 
-    internal abstract string Type { get; }
+//    internal abstract string Type { get; }
 
-    internal virtual void WriteTo(XmlWriter xmlWriter)
-    {
-      xmlWriter.WriteStartElement("xd", "descriptor", "http://schemas.microsoft.com/xmltools/2002/xmldiff");
-      xmlWriter.WriteAttributeString("opid", this._operationID.ToString());
-      xmlWriter.WriteAttributeString("type", this.Type);
-      xmlWriter.WriteEndElement();
-    }
-  }
-}
+//    internal virtual void WriteTo(XmlWriter xmlWriter)
+//    {
+//      xmlWriter.WriteStartElement("xd", "descriptor", "http://schemas.microsoft.com/xmltools/2002/xmldiff");
+//      xmlWriter.WriteAttributeString("opid", this._operationID.ToString());
+//      xmlWriter.WriteAttributeString("type", this.Type);
+//      xmlWriter.WriteEndElement();
+//    }
+//  }
+//}
